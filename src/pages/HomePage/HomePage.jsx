fe37f05ff34abade093dffbd3c34e8b65136c085
Juan Elias Carrillo Eslait <112365570+IngCarrillo12@users.noreply.github.com>
2023-11-23 16:16:19 -0500

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,6 @@ import { useSelector } from 'react-redux'
 import { getAllUsers } from '../../resources/Auth'
 import { useNavigate } from 'react-router-dom'
 export const HomePage = () => {
-  location.reload();
   const ofertas = useSelector(state=> state.ofertas)
   const user = useSelector(state=>state.user)
   const [quantyMarcas, setQuantyMarcas] = useState(0)
